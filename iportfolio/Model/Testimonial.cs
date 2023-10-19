4b3f0e47045fa98386da2f69cf81d288e3f7e199
HammadMaqbool <hammad.mqbl@gmail.com>
2023-10-19 20:28:54 +0500

    Update Testimonial.cs
    Don't make Image propertry required, Keep it  allow null

--- iportfolio/Model/Testimonial.cs
+++ iportfolio/Model/Testimonial.cs
@@ -1,23 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace iportfolio.Model
 {
     public class Testimonial
     {
-        internal object photo;
+       
 
         public int Id { get; set; }
 
         [Required(ErrorMessage ="Please Enter Name")]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "Please Enter Designation")]
         public string Designation { get; set; }
 
         [Required(ErrorMessage = "Please Enter Testimonial")]
         public string theTestimonial { get; set; }
-        [Required(ErrorMessage = "Please Enter Name")]
-        public string Image { get; set; }
+
+        //[Required(ErrorMessage = "Please Enter Name")]
+        //Error: If you will make it required form will not get submitted
+        public string? Image { get; set; }
+
         [NotMapped]
         [Required(ErrorMessage = "Please Select Photo ")]
         public IFormFile? Photo { get; set; }
